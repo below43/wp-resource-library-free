b0f6b60ca4f92ab7aaa7a2ca0681a8ac4ce8e555
Andrew Drake <andrew@below43.com>
2024-03-13 08:56:25 +1300

    Category list support

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ A simple, free, resource library plugin for Wordpress
 ### Slug
 Make sure the "resources" slug is free for this plugin to use.
 
-### Single resource file
-Copy single-resource.php into your theme folder
+### Resource posts in your theme
+Copy the theme samples from this project to your theme folder (remove the "sample-" prefix).
 
 This file should be placed in your active theme's directory. If you're using a child theme, place it in the child theme's directory.
 By default, WordPress uses the single.php template to display a single post. However, for custom post types, you need to create a new template file in your theme's directory.
--- wp-simple-resource-library.php
+++ wp-simple-resource-library.php
@@ -24,17 +24,17 @@ function create_resource_post_type()
 		'taxonomies' => array('category'),
 
 		'hierarchical' => false,
-        'public' => true,
-        'show_ui' => true,
-        'show_in_menu' => true,
-        'menu_position' => 5,
-        'show_in_admin_bar' => true,
-        'show_in_nav_menus' => true,
-        'can_export' => true,
-        'has_archive' => true,
-        'exclude_from_search' => false,
-        'publicly_queryable' => true,
-        'capability_type' => 'post',
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'menu_position' => 5,
+		'show_in_admin_bar' => true,
+		'show_in_nav_menus' => true,
+		'can_export' => true,
+		'has_archive' => true,
+		'exclude_from_search' => false,
+		'publicly_queryable' => true,
+		'capability_type' => 'post',
 	);
 
 	register_post_type('resource', $args);
@@ -61,47 +61,51 @@ function resource_details_meta_box_markup($object)
 	<input name="resource-url" type="text" class="large-text" value="<?php echo esc_html(get_post_meta($object->ID, "resource-url", true)); ?>">
 	<br />
 	<br />
-
+	<!--embed code -->
+	<label for="embed-code">Embed Code</label><br />
+	<textarea name="embed-code" rows="5" class="large-text"><?php echo esc_html(get_post_meta($object->ID, "embed-code", true)); ?></textarea>
+	<br />
+	<br />
 <?php
 }
 
 function audit_log_meta_box_markup($object)
 {
-    wp_nonce_field(basename(__FILE__), "audit-log-nonce");
+	wp_nonce_field(basename(__FILE__), "audit-log-nonce");
 
 ?>
-    <label for="dated">First Published</label><br />
-    <input name="created" type="date" value="<?php echo esc_html(get_post_meta($object->ID, "created", true)); ?>">
-    <br />
-    <br />
+	<label for="dated">Document Created</label><br />
+	<input name="created" type="date" value="<?php echo esc_html(get_post_meta($object->ID, "created", true)); ?>">
+	<br />
+	<br />
 
-    <label for="last-updated">Last Updated</label><br />
-    <input name="last-updated" type="date" value="<?php echo esc_html(get_post_meta($object->ID, "last-updated", true)); ?>">
-    <br />
-    <br />
+	<label for="last-updated">Last Updated</label><br />
+	<input name="last-updated" type="date" value="<?php echo esc_html(get_post_meta($object->ID, "last-updated", true)); ?>">
+	<br />
+	<br />
 
-    <label for="last-review-date">Last Date of Review</label><br />
-    <input name="last-review-date" type="date" value="<?php echo esc_html(get_post_meta($object->ID, "last-review-date", true)); ?>">
-    <br />
-    <br />
+	<label for="last-review-date">Last Date of Review</label><br />
+	<input name="last-review-date" type="date" value="<?php echo esc_html(get_post_meta($object->ID, "last-review-date", true)); ?>">
+	<br />
+	<br />
 
-    <label for="next-review-date">Next Date of Review</label><br />
-    <input name="next-review-date" type="date" value="<?php echo esc_html(get_post_meta($object->ID, "next-review-date", true)); ?>">
-    <br />
-    <br />
+	<label for="next-review-date">Next Date of Review</label><br />
+	<input name="next-review-date" type="date" value="<?php echo esc_html(get_post_meta($object->ID, "next-review-date", true)); ?>">
+	<br />
+	<br />
 
-    <label for="repealed-policies">Changelog</label><br />
-    <textarea name="changes" rows="5" class="large-text"><?php echo esc_html(get_post_meta($object->ID, "changelog", true)); ?></textarea><br />
-    eg. major changes, repealed policies, etc.
-    <br />
-    <br />
+	<label for="repealed-policies">Changelog</label><br />
+	<textarea name="changes" rows="5" class="large-text"><?php echo esc_html(get_post_meta($object->ID, "changelog", true)); ?></textarea><br />
+	eg. major changes, repealed policies, etc.
+	<br />
+	<br />
 <?php
 }
 
 function save_resource_metadata($post_id, $post, $update)
 {
 	$meta_boxes = array(
-		'resource-details' => array('resource-url'),
+		'resource-details' => array('resource-url', 'embed-code'),
 		'audit-log' => array('created', 'last-updated', 'last-review-date', 'next-review-date', 'changelog'),
 	);
 
@@ -133,90 +137,116 @@ function save_resource_metadata($post_id, $post, $update)
 
 add_action("save_post", "save_resource_metadata", 10, 3);
 
-function display_resources()
+function display_resources_item_shortcode($atts)
 {
-	$args = array(
-		'post_type' => 'resource',
-		'posts_per_page' => -1, // get all posts
+	// Extract the attributes
+	$atts = shortcode_atts(
+		array(
+			'id' => '', // Default value
+		),
+		$atts,
+		'display_resources_shortcode'
 	);
 
-	$the_query = new WP_Query($args);
-	$output = '';
+	$post = get_post($atts['id']); // Get the post with the ID from the shortcode attributes
+	setup_postdata($post); // Set up global post data
 
-	if ($the_query->have_posts()) {
-		while ($the_query->have_posts()) {
-			$the_query->the_post();
-			$output .= '<h2>' . get_the_title() . '</h2>';
-			$output .= '<p>Date: ' . get_the_date() . '</p>'; // Output the date
-			$output .= '<p>' . get_the_content() . '</p>';
-			$output .= '<p>Resource URL: ' . esc_html(get_post_meta(get_the_ID(), 'resource-url', true)) . '</p>';
-		}
-		/* Restore original Post Data */
-		wp_reset_postdata();
+	$output = '';
+	if ($post) {
+		$output .= '<h3 class="resource-item"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h3>';
+		$output .= '<p><small>Published: ' . get_the_date() . '</small></p>';
+		$output .= '<p>' . get_the_excerpt() . '</p>';
+		$output .= '<a href="' . get_permalink() . '">View more</a>';
 	} else {
-		$output = '<p>No resources found.</p>';
+		$output = '<p>No resource found.</p>';
 	}
+
+	wp_reset_postdata();
 
 	return $output;
 }
 
-function display_resources_shortcode($atts)
+add_shortcode('display_resource_item', 'display_resources_item_shortcode');
+
+
+function display_resource_shortcode($atts)
 {
-	return display_resources();
+	// Extract the attributes
+	$atts = shortcode_atts(
+		array(
+			'id' => '', // Default value
+		),
+		$atts,
+		'display_resource_shortcode'
+	);
+
+	ob_start();
+
+	$post = get_post($atts['id']); // Get the post with the ID from the shortcode attributes
+	setup_postdata($post); // Set up global post data
+?>
+	<h1><?php the_title(); ?></h1>
+	<p>Published: <?php echo get_the_date(); ?><br />
+		Categories: <?php echo get_the_category_list(', '); ?></p>
+
+	<?php the_excerpt(); ?>
+
+	<!-- if this has embed code then output it below -->
+	<?php
+	$embed_code = get_post_meta(get_the_ID(), 'embed-code', true);
+	if (!empty($embed_code)) {
+		echo '<div class="embed-code">' . $embed_code . '</div>';
+	}
+	?>
+
+	<?php
+	$url = get_post_meta(get_the_ID(), 'resource-url', true);
+	if (!empty($url)) {
+		echo '<p><a class="button" href="' . esc_html($url) . '" target="_blank">Open this resource</a></p>';
+	}
+	else 
+	{
+		echo '<p>No URL provided for this resource.</p>';
+	}
+
+	$created = get_post_meta(get_the_ID(), 'created', true);
+	$last_updated = get_post_meta(get_the_ID(), 'last-updated', true);
+	$last_review_date = get_post_meta(get_the_ID(), 'last-review-date', true);
+	$next_review_date = get_post_meta(get_the_ID(), 'next-review-date', true);
+	$changelog = get_post_meta(get_the_ID(), 'changelog', true);
+
+	if (!empty($created) || !empty($last_updated) || !empty($last_review_date) || !empty($next_review_date) || !empty($changelog)) : ?>
+		<hr>
+		<div class="audit-details">
+			<?php if (!empty($created)) : ?>
+				<p>Created: <?php echo esc_html($created); ?></p>
+			<?php endif; ?>
+			<?php if (!empty($last_updated)) : ?>
+				<p>Last Updated: <?php echo esc_html($last_updated); ?></p>
+			<?php endif; ?>
+			<?php if (!empty($last_review_date)) : ?>
+				<p>Last Date of Review: <?php echo esc_html($last_review_date); ?></p>
+			<?php endif; ?>
+			<?php if (!empty($next_review_date)) : ?>
+				<p>Next Date of Review: <?php echo esc_html($next_review_date); ?></p>
+			<?php endif; ?>
+			<?php if (!empty($changelog)) : ?>
+				<p>Changelog: <?php echo esc_html($changelog); ?></p>
+			<?php endif; ?>
+		</div>
+<?php endif;
+
+	wp_reset_postdata();
+
+	return ob_get_clean();
 }
 
-add_shortcode('display_resources', 'display_resources_shortcode');
-
-
-function display_resource_shortcode() {
-    ob_start();
-
-    $args = array(
-        'post_type' => 'resource',
-        'posts_per_page' => -1
-    );
-    $query = new WP_Query($args);
-
-    while ( $query->have_posts() ) : $query->the_post(); ?>
-
-        <h1><?php the_title(); ?></h1>
-        <p>Categories: <?php echo get_the_category_list(', '); ?></p>
-
-        <?php the_excerpt(); ?>
-
-        <a href="<?php echo esc_html(get_post_meta(get_the_ID(), 'resource-url', true)); ?>" target="_blank">Open Resource</a>
-
-        <?php
-        $created = get_post_meta(get_the_ID(), 'created', true);
-        $last_updated = get_post_meta(get_the_ID(), 'last-updated', true);
-        $last_review_date = get_post_meta(get_the_ID(), 'last-review-date', true);
-        $next_review_date = get_post_meta(get_the_ID(), 'next-review-date', true);
-        $changelog = get_post_meta(get_the_ID(), 'changelog', true);
-
-        if (!empty($created) || !empty($last_updated) || !empty($last_review_date) || !empty($next_review_date) || !empty($changelog)) : ?>
-            <hr>
-            <div class="audit-details">
-                <?php if (!empty($created)) : ?>
-                    <p>First Published: <?php echo esc_html($created); ?></p>
-                <?php endif; ?>
-                <?php if (!empty($last_updated)) : ?>
-                    <p>Last Updated: <?php echo esc_html($last_updated); ?></p>
-                <?php endif; ?>
-                <?php if (!empty($last_review_date)) : ?>
-                    <p>Last Date of Review: <?php echo esc_html($last_review_date); ?></p>
-                <?php endif; ?>
-                <?php if (!empty($next_review_date)) : ?>
-                    <p>Next Date of Review: <?php echo esc_html($next_review_date); ?></p>
-                <?php endif; ?>
-                <?php if (!empty($changelog)) : ?>
-                    <p>Changelog: <?php echo esc_html($changelog); ?></p>
-                <?php endif; ?>
-            </div>
-        <?php endif;
-
-    endwhile;
-    wp_reset_postdata();
-
-    return ob_get_clean();
-}
 add_shortcode('display_resource', 'display_resource_shortcode');
+
+function include_resources_in_category_pages($query)
+{
+	if ($query->is_category() && $query->is_main_query()) {
+		$query->set('post_type', array('post', 'resource'));
+	}
+}
+add_action('pre_get_posts', 'include_resources_in_category_pages');
